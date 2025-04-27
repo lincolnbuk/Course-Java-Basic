4ec3cb58dac52bb1e8d65a34722f83d754d5e3d1
Adriano Lincoln <sit-bull@hotmail.com>
2025-04-27 17:10:40 -0300

    Class Matrizes created

---
+++ src/Matrizes.java
@@ -0,0 +1,22 @@
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        double[][] notasAlunos = new double[30][4];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 8;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 8;
+        notasAlunos[1][1] = 7.5;
+        notasAlunos[1][2] = 9;
+        notasAlunos[1][3] = 8.3;
+
+        notasAlunos[2][0] = 8;
+        notasAlunos[2][1] = 7;
+        notasAlunos[2][2] = 9;
+        notasAlunos[2][3] = 10;
+    }
+}
