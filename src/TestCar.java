b824deab3a42a4ead5808ef4c397dd461a1cbea5
Adriano Lincoln <sit-bull@hotmail.com>
2025-05-01 19:28:57 -0300

    Private and Public Modifiers

---
+++ src/TestCar.java
@@ -0,0 +1,11 @@
+public class TestCar {
+
+    public static void main(String[] args) {
+
+        Car van = new Car();
+
+        van.setMarca("Fiat");
+
+        System.out.println(van.getMarca());
+    }
+}
