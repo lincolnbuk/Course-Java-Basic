99c599ad047379d9d84eed7b73a9b6ef0981f543
Adriano Lincoln <sit-bull@hotmail.com>
2025-05-01 20:28:27 -0300

    Recursion - Fibonacci

---
+++ src/TesteCalc.java
@@ -0,0 +1,9 @@
+public class TesteCalc {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<20; i++){
+            System.out.print(Calculadora.fibonacci(i) + " ");
+        }
+    }
+}
